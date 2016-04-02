ffa2d5bd0d4220eea50547b9858667d708b07556
Cole R Lawrence <Colelawr@gmail.com>
2016-04-02 17:50:15 -0500

    Create a hadoop config folder and variables for it

--- hamlet-test.cmd
+++ hamlet-test.cmd
@@ -1,1 +1,8 @@
-".\hadoop-2.6.4\bin\hadoop" jar hadoop/target/ngram-jam-project-0.0.1-SNAPSHOT.jar edu.missouristate.csc333.ngramjam input/hamlet.txt output
+@echo off
+setlocal EnableDelayedExpansion
+set PROJECT_LOC=%~dp0
+set HADOOP_PREFIX=%PROJECT_LOC%hadoop-2.6.4
+set HADOOP_CONF_DIR=%PROJECT_LOC%\config\hadoop
+set YARN_CONF_DIR=%HADOOP_CONF_DIR%
+set PATH=%PATH%;%HADOOP_PREFIX%\bin
+hadoop jar hadoop/target/ngram-jam-project-0.0.1-SNAPSHOT.jar edu.missouristate.csc333.ngramjam input/hamlet.txt output
